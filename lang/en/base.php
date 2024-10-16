5245d052f71825d9d53287b0ff221b38debb6a5d
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-16 19:58:03 +0330

    added language key

---
+++ lang/en/base.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Pax Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Pax for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'description' => 'A secure and intuitive currency exchange platform designed to facilitate seamless transactions and promote peace of mind in financial exchanges.',
+
+    'sections' => [
+        'content' => [
+            'name' => 'Content',
+            'title' => 'Content Management',
+        ],
+        'sell' => [
+            'name' => 'Sell',
+            'title' => 'Sell Management',
+        ],
+        'account' => [
+            'name' => 'Accounts',
+            'title' => 'Account Management',
+        ],
+        'system' => [
+            'name' => 'System',
+            'title' => 'System Management',
+        ],
+        'report' => [
+            'name' => 'Reports',
+            'title' => 'Management Reports',
+        ],
+    ],
+
+];
